921f465e40c4d0aba36e5e43d33be9aa4e27fee9
Soleiman Omidvari <omidvarisl@gmail.com>
2025-12-01 14:13:17 +0100

    Update index.jschange

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const cors = require("cors");
-
 const port = process.env.PORT || 8080;
 
-server.use(cors());
+
+server.use('/images', express.static('public/images'));
+
 server.use(middlewares);
 server.use(router);
 
 server.listen(port, () => {
-  console.log("JSON Server is running on port " + port);
+    console.log("Server is running");
 });
